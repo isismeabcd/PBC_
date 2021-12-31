f3569b75750a9b56243a329dd32948a97b04f22f
Jackie890621 <jackie890621@gmail.com>
2021-12-31 22:21:16 +0800

    update3

--- try.py
+++ try.py
@@ -1,3 +1,4 @@
+from tkinter import *
 import tkinter as tk
 import tkinter.font as tkFont
 import PIL
@@ -29,27 +30,27 @@ class maketree(tk.Frame):
         self.l_pickfile = tk.Label(self, text = '請上傳檔案：', fg = 'black', font = f2)
         self.b_pickfile = tk.Button(self, text = '選擇檔案', bg = 'gray', fg = 'black', command = self.clickb_p, font = f2)
         self.l_filecheck = tk.Label(self, text = '您尚未選擇檔案', fg = 'gray', font = f1)
-        self.l_setting = tk.Label(self, text = '資料處理設定', bg = 'Gainsboro', fg = 'black', font = f2)
+        self.l_setting = tk.Label(self, text = '資料處理設定', bg = 'Gainsboro', fg = 'black', font = f2, height = 2)
         
         remind = '* 如未進行設定，則預設為：剔除離群值、以KNN Imputer方式補齊缺失值、進行降維處理。'
         self.l_remind = tk.Label(self, text = remind, bg = 'LightCoral', fg = 'Snow', font = f1)
     #第一題：離群值
-        self.l_Q1 = tk.Label(self, text = '1. 是否要踢除離群值(outlier)？', font = f4)
-        self.Q1_1 = tk.Radiobutton(self, text = '是，以缺失值處理', variable = radioValue, value = 1 , font = f4)
-        self.Q1_2 = tk.Radiobutton(self, text = '否，保留離群值', variable = radioValue, value = 2, font = f4)
-        self.Q1_check = tk.Button(self, text = '檢視', bg = 'gray', fg = 'black', font = f4)
+        self.l_Q1 = tk.Label(self, text = '1. 是否要踢除離群值(outlier)？', font = f4, height = 2)
+        self.Q1_1 = tk.Radiobutton(self, text = '是，以缺失值處理', variable = radioValue, value = 1, font = f4, height = 2)
+        self.Q1_2 = tk.Radiobutton(self, text = '否，保留離群值', variable = radioValue, value = 2, font = f4, height = 2)
+        self.Q1_check = tk.Button(self, text = '檢視', bg = 'gray', fg = 'black', font = f4, height = 1)
         #第二題：缺失值
-        self.l_Q2 = tk.Label(self, text = '2. 缺失值替補方式？', font = f4)
-        self.Q2_1 = tk.Radiobutton(self, text = '平均值', variable = radioValue2, value = 1 , font = f4)
-        self.Q2_2 = tk.Radiobutton(self, text = '中位數', variable = radioValue2, value = 2, font = f4)
-        self.Q2_3 = tk.Radiobutton(self, text = '眾數', variable = radioValue2, value = 3 , font = f4)
-        self.Q2_4 = tk.Radiobutton(self, text = 'KNN Imputer', variable = radioValue2, value = 4, font = f4)
-        self.Q2_check = tk.Button(self, text = '檢視', bg = 'gray', fg = 'black', font = f4)
+        self.l_Q2 = tk.Label(self, text = '2. 缺失值替補方式？', font = f4, height = 4)
+        self.Q2_1 = tk.Radiobutton(self, text = '平均值', variable = radioValue2, value = 1 , font = f4, height = 1)
+        self.Q2_2 = tk.Radiobutton(self, text = '中位數', variable = radioValue2, value = 2, font = f4, height = 1)
+        self.Q2_3 = tk.Radiobutton(self, text = '眾數', variable = radioValue2, value = 3 , font = f4, height = 1)
+        self.Q2_4 = tk.Radiobutton(self, text = 'KNN Imputer', variable = radioValue2, value = 4, font = f4, height = 1)
+        self.Q2_check = tk.Button(self, text = '檢視', bg = 'gray', fg = 'black', font = f4, height = 1)
 
         #第三題：降維度
-        self.l_Q3 = tk.Label(self, text = '3. 是否要將資料降維處理(PCA)？', font = f4)
-        self.Q3_1 = tk.Radiobutton(self, text = '是', variable = radioValue3, value = 1 , font = f4)
-        self.Q3_2 = tk.Radiobutton(self, text = '否', variable = radioValue3, value = 2, font = f4)
+        self.l_Q3 = tk.Label(self, text = '3. 是否要將資料降維處理(PCA)？', font = f4, height = 2)
+        self.Q3_1 = tk.Radiobutton(self, text = '是', variable = radioValue3, value = 1 , font = f4, height = 2)
+        self.Q3_2 = tk.Radiobutton(self, text = '否', variable = radioValue3, value = 2, font = f4, height = 2)
     
         #輸完資料後開始進行處理
         self.b_continue = tk.Button(self, text = '下一步', command = self.click_con, font = f2)
@@ -58,7 +59,7 @@ class maketree(tk.Frame):
         self.l_pickfile.grid(row = 4, column = 1, columnspan = 10, sticky = tk.S)
         self.b_pickfile.grid(row = 4, column = 11, columnspan = 5, sticky = tk.S + tk.E + tk.W)
         self.l_filecheck.grid(row = 4, column = 16, columnspan = 30, sticky = tk.S)
-        self.l_setting.grid(row = 5, column = 1, columnspan = 30, sticky = tk.SW + tk.NE)
+        self.l_setting.grid(row = 5, column = 1, columnspan = 30, sticky = tk.S)
         self.l_remind.grid(row = 6, column = 1, columnspan = 30, sticky = tk.SW + tk.NE)
 
         self.l_Q1.grid(row = 7, column = 1, columnspan = 10, sticky = tk.SW)
@@ -66,23 +67,25 @@ class maketree(tk.Frame):
         self.Q1_2.grid(row = 7, column = 18, columnspan = 7, sticky = tk.SW)
         self.Q1_check.grid(row = 7, column = 25, columnspan = 6, sticky = tk.W + tk.E)
 
-        self.l_Q2.grid(row = 8, rowspan = 2, column = 1, columnspan = 10, sticky = tk.W)
-        self.Q2_1.grid(row = 8, column = 11, columnspan = 7, sticky = tk.SW)
-        self.Q2_2.grid(row = 8, column = 18, columnspan = 7, sticky = tk.SW)
-        self.Q2_3.grid(row = 9, column = 11, columnspan = 7, sticky = tk.SW)
-        self.Q2_4.grid(row = 9, column = 18, columnspan = 7, sticky = tk.SW)
-        self.Q2_check.grid(row = 8, rowspan = 2, column = 25, columnspan = 6, sticky = tk.W + tk.E)
+        self.l_Q2.grid(row = 9, rowspan = 2, column = 1, columnspan = 10, sticky = tk.W)
+        self.Q2_1.grid(row = 9, column = 11, columnspan = 7, sticky = tk.SW)
+        self.Q2_2.grid(row = 9, column = 18, columnspan = 7, sticky = tk.SW)
+        self.Q2_3.grid(row = 10, column = 11, columnspan = 7, sticky = tk.NW)
+        self.Q2_4.grid(row = 10, column = 18, columnspan = 7, sticky = tk.NW)
+        self.Q2_check.grid(row = 9, rowspan = 2, column = 25, columnspan = 6, sticky = tk.W + tk.E)
 
-        self.l_Q3.grid(row = 10, column = 1, columnspan = 10, sticky = tk.SW)
-        self.Q3_1.grid(row = 10, column = 11, columnspan = 7, sticky = tk.SW)
-        self.Q3_2.grid(row = 10, column = 18, columnspan = 7, sticky = tk.SW)
+        self.l_Q3.grid(row = 12, column = 1, columnspan = 10, sticky = tk.SW)
+        self.Q3_1.grid(row = 12, column = 11, columnspan = 7, sticky = tk.SW)
+        self.Q3_2.grid(row = 12, column = 18, columnspan = 7, sticky = tk.SW)
 
-        self.b_continue.grid(row = 15, column = 1, columnspan = 3, sticky = tk.SW + tk.NE)
+        self.b_continue.grid(row = 17, column = 1, columnspan = 3, sticky = tk.SW + tk.NE)
 
     #讓使用者找檔案
     def readfile(self):
-        Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
+        root = Tk()
+        root.withdraw()# we don't want a full GUI, so keep the root window from appearing
         filename = askopenfilename(filetypes = [("txt files", "*.txt"), ("excel files", "*.csv"), ("excel files", "*.xlsx")])
+        root.destroy()
         return filename 
         #file_name是路徑，之後需要可以叫它
 
@@ -130,12 +133,3 @@ class maketree(tk.Frame):
 tree = maketree()
 tree.master.title('The Decision Tree Tool')
 tree.mainloop()
-
-
-
-
-
-
-
-
-
